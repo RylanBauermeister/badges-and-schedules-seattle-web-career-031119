67fdc5b89e6df9ecd5e465174aa6af6571277d7f
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 19:47:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  badges = Array.new
+  for |name| in array
+    badges << badge_maker(name)
+  end
+  badges
+end

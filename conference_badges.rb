a9955c95243ac075472b9c688cbf66d1add2c6af
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 19:50:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(array)
   assignments = Array.new
   for room in 0...8
-    assignments << "Helllo, #{array[i]}! You'll be assigned to room #{i+1}!"
+    assignments << "Helllo, #{array[room]}! You'll be assigned to room #{room+1}!"
   end
   assignments
   
